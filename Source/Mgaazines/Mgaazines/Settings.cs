3aef86c41a953b40cd97e57881a4f5b848e8b4d7
Caulaflower <86428032+Caulaflower@users.noreply.github.com>
2021-10-31 22:45:54 +0100

    Add files via upload

---
+++ Source/Mgaazines/Mgaazines/Settings.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using UnityEngine; 
+
+namespace Mgaazines
+{
+    public class BipodSettings : ModSettings
+    {
+       
+        public bool NerfBuffBool;
+        public bool Testingmode;
+
+
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref NerfBuffBool, "NerfBuffBool");
+            Scribe_Values.Look(ref Testingmode, "testmode");
+
+            base.ExposeData();
+        }
+    }
+
+    public class BipodMod : Mod
+    {
+       
+        BipodSettings settings;
+
+        
+        public BipodMod(ModContentPack content) : base(content)
+        {
+            this.settings = GetSettings<BipodSettings>();
+        }
+
+        
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listingStandard = new Listing_Standard();
+            listingStandard.Begin(inRect);
+            if (settings.NerfBuffBool)
+            {
+                listingStandard.CheckboxLabeled("Make bipods a nerf (guns without bipod setup have 2 of normal warmup time, with they have 1)", ref settings.NerfBuffBool, "Make bipods a nerf");
+            }
+            if (!settings.NerfBuffBool)
+            {
+                listingStandard.CheckboxLabeled("Make bipods a buff (guns without bipod set up have normal warmup time, with they have half of it) ", ref settings.NerfBuffBool, "Make bipods a buff");
+            }
+            if (settings.Testingmode)
+            {
+                listingStandard.CheckboxLabeled("Turn off testing mode (enable Log.Error and Log.Message parst of code)", ref settings.Testingmode, "Turn off testing mode");
+            }
+            if (!settings.Testingmode)
+            {
+                listingStandard.CheckboxLabeled("Turn on testing mode (enable Log.Error and Log.Message parst of code)", ref settings.Testingmode, "Turn on testing mode");
+            }
+
+
+            listingStandard.End();
+            base.DoSettingsWindowContents(inRect);
+        }
+
+       
+        public override string SettingsCategory()
+        {
+            return "Bipod mod".Translate();
+        }
+    }
+}
---
+++ Source/Mgaazines/Mgaazines/maincode.cs
@@ -0,0 +1,425 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CombatExtended;
+using RimWorld;
+using Verse;
+using Verse.AI;
+using Verse.Sound;
+using UnityEngine;
+
+
+namespace Mgaazines
+{
+	[DefOf]
+	public class BipodDefsOfs : DefOf 
+	{
+		public static JobDef setupbipodjobdef;
+	}
+	
+	public class BipedProps : CompProperties
+	{
+
+
+		public int additionalrange = 12;
+
+		public float recoilmulton = 0.5f;
+
+		public float recoilmultoff = 1f;
+
+		public float warmupmult = 0.85f;
+
+		public float warmuppenalty = 2f;
+
+		public int TicksToSetUp = 60;
+	
+		
+
+		public BipedProps()
+		{
+			this.compClass = typeof(bipodcomp);
+		}
+
+		public BipedProps(Type compClass) : base(compClass)
+		{
+			this.compClass = compClass;
+		}
+
+	}
+	public class bipodcomp : CompRangedGizmoGiver
+	{
+		public bool ShouldSetUp;
+
+		public bool IsSetUpRn;
+
+		public ThingWithComps BipodAttached;
+
+		public override void PostExposeData()
+		{
+			Scribe_References.Look(ref BipodAttached, "BipodAttached");
+			base.PostExposeData();
+		}
+		public override IEnumerable<Gizmo> CompGetGizmosExtra()
+		{
+			if (!ShouldSetUp)
+			{
+				yield return new Command_Action
+				{
+					action = delegate { ShouldSetUp = true; },
+					defaultLabel = "Set up bipod",
+					icon = ContentFinder<Texture2D>.Get("Tymon/Bipod/open_bipod")
+				};
+			}
+			else
+			{
+				yield return new Command_Action
+				{
+					action = delegate { ShouldSetUp = false; },
+					defaultLabel = "Close bipod",
+					icon = ContentFinder<Texture2D>.Get("Tymon/Bipod/closed_bipod")
+				};
+
+			}
+			
+		}
+		public VerbBipodShoot verbbipod
+		{
+			get
+			{
+				var result = (VerbBipodShoot)this.parent.TryGetComp<CompEquippable>().PrimaryVerb;
+				return result;
+			}
+		}
+		public override void Notify_Unequipped(Pawn pawn)
+		{
+			
+			IsSetUpRn = false;
+		}
+		public override void Notify_Equipped(Pawn pawn)
+		{
+			IsSetUpRn = false;
+			verbbipod.WereChangesApplied1 = true;
+			verbbipod.WereChangesApplied2 = true;
+		}
+
+		public BipedProps Props => (BipedProps)this.props;
+		public void SetUpStart(Pawn pawn = null)
+		{
+			//Log.Message("der panzerwagen");
+			if(pawn != null)
+			{
+				IntVec3 pawnpos = pawn.Position;
+				List<IntVec3> pawnsurs = pawn.CellsAdjacent8WayAndInside().ToList();
+				pawnsurs.Remove(pawnpos);
+				var surthings = new List<Thing>();
+				foreach(IntVec3 cell in pawnsurs)
+				{
+					surthings.AddRange(cell.GetThingList(Find.CurrentMap));
+				}
+				pawn.jobs.StopAll();
+				pawn.jobs.StartJob(new Job { def = BipodDefsOfs.setupbipodjobdef, targetA = this.parent}, JobCondition.InterruptForced);
+			}
+
+		}
+
+	}
+
+	public class VerbBipodShoot : Verb_ShootCE
+	{
+		public override void WarmupComplete()
+		{
+			
+			
+			base.WarmupComplete();
+		}
+		public int ticks;
+
+		public bool WereChangesApplied1 = false;
+
+		public bool WereChangesApplied2 = false;
+
+		public VerbPropertiesCE verpbrops
+		{
+			get
+			{
+				return (VerbPropertiesCE)this.EquipmentSource.def.Verbs.Find(tt22 => tt22.verbClass == typeof(VerbBipodShoot));
+			}
+		}
+
+		public bipodcomp bipodcomp
+		{
+			get
+			{
+				return this.EquipmentSource.TryGetComp<bipodcomp>();
+			}
+		}
+
+		public override void VerbTickCE()
+		{
+			++ticks;
+			if(ticks == 2)
+			{
+				if (!(CasterPawn.ParentHolder is Map))
+				{
+					return;
+				}
+				if (CasterPawn != null)
+				{
+					if (!CasterPawn.pather.Moving && EquipmentSource.TryGetComp<bipodcomp>().IsSetUpRn)
+					{
+						if (!WereChangesApplied1)
+						{
+							VerbPropertiesCE VerbPropsClone = (VerbPropertiesCE)this.verbProps.MemberwiseClone();
+							VerbPropsClone.recoilAmount = verpbrops.recoilAmount * bipodcomp.Props.recoilmulton;
+							VerbPropsClone.warmupTime = verpbrops.warmupTime * bipodcomp.Props.warmupmult;
+							VerbPropsClone.range += EquipmentSource.TryGetComp<bipodcomp>().Props.additionalrange;
+							this.verbProps = VerbPropsClone;
+							WereChangesApplied1 = true;
+						}
+
+
+					}
+					if (!CasterPawn.pather.Moving && !EquipmentSource.TryGetComp<bipodcomp>().IsSetUpRn)
+					{
+						if (!WereChangesApplied2)
+						{
+							VerbPropertiesCE VerbPropsClone = (VerbPropertiesCE)this.verbProps.MemberwiseClone();
+							VerbPropsClone.recoilAmount = verpbrops.recoilAmount * bipodcomp.Props.recoilmultoff;
+							VerbPropsClone.warmupTime = verpbrops.warmupTime * bipodcomp.Props.warmuppenalty;
+							VerbPropsClone.range = verpbrops.range;
+							this.verbProps = VerbPropsClone;
+							WereChangesApplied2 = true;
+						}
+
+					}
+					if (!CasterPawn.pather.Moving && EquipmentSource.TryGetComp<bipodcomp>().ShouldSetUp && CasterPawn.CurJob.def != BipodDefsOfs.setupbipodjobdef && !bipodcomp.IsSetUpRn)
+					{
+						EquipmentSource.TryGetComp<bipodcomp>().SetUpStart(CasterPawn);
+					}
+					if (CasterPawn.pather.Moving)
+					{
+						WereChangesApplied1 = false;
+						WereChangesApplied2 = false;
+						EquipmentSource.TryGetComp<bipodcomp>().IsSetUpRn = false;
+						this.verbProps = EquipmentSource.def.Verbs.Find(tt33 => tt33.verbClass == typeof(VerbBipodShoot));
+					}
+				}
+				ticks = 0;
+			}
+		
+			
+			base.VerbTickCE();
+		}
+	}
+
+
+	
+	public class SetUpBipod : JobDriver
+	{
+		private ThingWithComps weapon
+		{
+			get
+			{
+				return base.TargetThingA as ThingWithComps;
+			}
+		}
+
+		private const TargetIndex guntosetup = TargetIndex.A;
+		public override bool TryMakePreToilReservations(bool errorOnFailed)
+		{
+			//return true;	
+			return this.pawn.Reserve(this.job.GetTarget(guntosetup), this.job, 1, -1, null);
+		}
+		public bipodcomp Bipod
+		{
+			get
+			{
+				return this.weapon.TryGetComp<bipodcomp>();
+			}
+		}
+		protected override IEnumerable<Toil> MakeNewToils()
+		{
+			yield return Toils_General.Wait(Bipod.Props.TicksToSetUp);
+			yield return Toils_General.Do(delegate { Bipod.IsSetUpRn = true; });
+
+			
+		}
+	}
+	[StaticConstructorOnStartup]
+	public class AddChanger
+	{
+		static AddChanger()
+		{
+			Add_and_Change_LMGs();
+			Add_and_Change_DMRs();
+			Add_and_Change_ATRs();
+		}
+		public static void Add_and_Change_ATRs()
+		{
+			List<ThingDef> defs = DefDatabase<ThingDef>.AllDefsListForReading.FindAll(k => (k.weaponTags?.Any(O => O == "Bipod_ATR") ?? false) && (!k.Verbs?.Any(P => P.verbClass == typeof(VerbBipodShoot)) ?? false));
+			foreach (ThingDef def in defs)
+			{
+				Log.Message("adding bipod (ATR) to: " + def.defName);
+				if (def.Verbs?.Any(PP => PP.verbClass == typeof(Verb_ShootCE)) ?? false)
+				{
+					var dar = def.Verbs.Find(PP => PP.verbClass == typeof(Verb_ShootCE)).MemberwiseClone();
+
+					if (dar != null)
+					{
+						//Log.Message("2");
+						dar.verbClass = typeof(VerbBipodShoot);
+						//Log.Message("3");
+						def.Verbs.Clear();
+						//Log.Message("4");
+						def.comps.Add(new BipedProps { additionalrange = 0, recoilmulton = 1f, recoilmultoff = 2.5f, TicksToSetUp = 480, warmupmult = 0.92f, warmuppenalty = 2.1f });
+						//Log.Message("5");
+						def.Verbs.Add(dar);
+						//Log.Message("6");
+						Log.Message("sucessfully added bipod (ATR) to: " + def.label);
+					}
+					else
+					{
+						Log.Message("adding bipod failed in " + def.label + ". It appears to have no VerbShootCE in verbs");
+					}
+				}
+				else
+				{
+					Log.Message("adding bipod failed in " + def.label + ". It appears to have no VerbShootCE in verbs");
+					foreach (VerbProperties verbp in def.Verbs)
+					{
+						Log.Message(verbp.verbClass.Name);
+					}
+
+				}
+
+
+			}
+		}
+		public static void Add_and_Change_DMRs()
+		{
+			List<ThingDef> defs = DefDatabase<ThingDef>.AllDefsListForReading.FindAll(k => (k.weaponTags?.Any(O => O == "Bipod_DMR") ?? false) && (!k.Verbs?.Any(P => P.verbClass == typeof(VerbBipodShoot)) ?? false));
+			foreach (ThingDef def in defs)
+			{
+				Log.Message("adding bipod (DMR) to: " + def.defName);
+				if (def.Verbs?.Any(PP => PP.verbClass == typeof(Verb_ShootCE)) ?? false)
+				{
+					var dar = def.Verbs.Find(PP => PP.verbClass == typeof(Verb_ShootCE)).MemberwiseClone();
+
+					if (dar != null)
+					{
+						//Log.Message("2");
+						dar.verbClass = typeof(VerbBipodShoot);
+						//Log.Message("3");
+						def.Verbs.Clear();
+						//Log.Message("4");
+						def.comps.Add(new BipedProps { additionalrange = 15, recoilmulton = 0.75f, recoilmultoff = 1f, TicksToSetUp = 240, warmupmult = 0.76f, warmuppenalty = 1.0f });
+						//Log.Message("5");
+						def.Verbs.Add(dar);
+						//Log.Message("6");
+						Log.Message("sucessfully added bipod (DMR) to: " + def.label);
+					}
+					else
+					{
+						Log.Message("adding bipod failed in " + def.label + ". It appears to have no VerbShootCE in verbs");
+					}
+				}
+				else
+				{
+					Log.Message("adding bipod failed in " + def.label + ". It appears to have no VerbShootCE in verbs");
+					foreach (VerbProperties verbp in def.Verbs)
+					{
+						Log.Message(verbp.verbClass.Name);
+					}
+
+				}
+
+
+			}
+		}
+		public static void Add_and_Change_LMGs()
+		{
+			List<ThingDef> defs = DefDatabase<ThingDef>.AllDefsListForReading.FindAll(k => (k.weaponTags?.Any(O => O == "Bipod_LMG") ?? false) && (!k.Verbs?.Any(P => P.verbClass == typeof(VerbBipodShoot)) ?? false));
+			foreach (ThingDef def in defs)
+			{
+				Log.Message("adding bipod to: " + def.label);
+				if (def.Verbs?.Any(PP => PP.verbClass == typeof(Verb_ShootCE)) ?? false)
+				{
+					var dar = def.Verbs.Find(PP => PP.verbClass == typeof(Verb_ShootCE)).MemberwiseClone();
+
+					if (dar != null)
+					{
+						//Log.Message("2");
+						dar.verbClass = typeof(VerbBipodShoot);
+						//Log.Message("3");
+						def.Verbs.Clear();
+						//Log.Message("4");
+						def.comps.Add(new BipedProps { additionalrange = 2, recoilmulton = 0.90f, recoilmultoff = 1.5f, TicksToSetUp = 360, warmupmult = 0.85f, warmuppenalty = 1.5f });
+						//Log.Message("5");
+						def.Verbs.Add(dar);
+						//Log.Message("6");
+						Log.Message("sucessfully added bipod (LMG) to: " + def.label);
+					}
+					else
+					{
+						Log.Message("adding bipod failed in " + def.label + ". It appears to have no VerbShootCE in verbs");
+					}
+				}
+				else
+				{
+					Log.Message("adding bipod failed in " + def.label + ". It appears to have no VerbShootCE in verbs");
+					foreach (VerbProperties verbp in def.Verbs)
+					{
+						Log.Message(verbp.verbClass.Name);
+					}
+
+				}
+
+
+			}
+		}
+		public static void Add_and_Change_Saws()
+		{
+			List<ThingDef> defs = DefDatabase<ThingDef>.AllDefsListForReading.FindAll(k => (k.weaponTags?.Any(O => O == "Bipod_SAW") ?? false) && (!k.Verbs?.Any(P => P.verbClass == typeof(VerbBipodShoot)) ?? false));
+			foreach (ThingDef def in defs)
+			{
+				Log.Message("adding bipod to: " + def.label);
+				if (def.Verbs?.Any(PP => PP.verbClass == typeof(Verb_ShootCE)) ?? false)
+				{
+					var dar = def.Verbs.Find(PP => PP.verbClass == typeof(Verb_ShootCE)).MemberwiseClone();
+
+					if (dar != null)
+					{
+						//Log.Message("2");
+						dar.verbClass = typeof(VerbBipodShoot);
+						//Log.Message("3");
+						def.Verbs.Clear();
+						//Log.Message("4");
+						def.comps.Add(new BipedProps { additionalrange = 4, recoilmulton = 0.90f, recoilmultoff = 1.2f, TicksToSetUp = 240, warmupmult = 0.90f, warmuppenalty = 1.2f });
+						//Log.Message("5");
+						def.Verbs.Add(dar);
+						//Log.Message("6");
+						Log.Message("sucessfully added bipod (SAW) to: " + def.label);
+					}
+					else
+					{
+						Log.Message("adding bipod failed in " + def.label + ". It appears to have no VerbShootCE in verbs");
+					}
+				}
+				else
+				{
+					Log.Message("adding bipod failed in " + def.label + ". It appears to have no VerbShootCE in verbs");
+					foreach (VerbProperties verbp in def.Verbs)
+					{
+						Log.Message(verbp.verbClass.Name);
+					}
+
+				}
+
+
+			}
+		}
+	}
+	
+}
